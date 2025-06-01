c038122e0c9e8a167cd305445fdec9b543c69142
Steven111105 <stevenwijaya1111@gmail.com>
2025-06-02 02:55:28 +0700

    Added UI for showing how to cast which spell

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -32,7 +32,7 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(timer > 20f){
+        if(timer > 15f){
             Destroy(gameObject);
         }
         timer += Time.deltaTime;
